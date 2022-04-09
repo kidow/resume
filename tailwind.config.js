f162e56fb4ae524f9f435d541c8668a9e6a65963
kidow <wcgo2ling@gmail.com>
2022-04-10 02:38:15 +0900

    나머지 파일들

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,20 @@ module.exports = {
     require("prettier-plugin-tailwindcss"),
     plugin(function ({ addComponents, theme }) {
       addComponents({
-        ".tag": {},
+        ".tag": {
+          paddingTop: theme("spacing.[0.5]"),
+          paddingBottom: theme("spacing.[0.5]"),
+          paddingLeft: theme("spacing.2"),
+          paddingRight: theme("spacing.2"),
+          borderRadius: theme("borderRadius.xl"),
+          fontSize: theme("fontSize.sm"),
+          backgroundColor: theme("backgroundColor.gray.800"),
+          color: theme("colors.gray.300"),
+        },
+        ".content": {
+          color: theme("colors.neutral.400"),
+          lineHeight: theme("lineHeight.8"),
+        },
       });
     }),
   ],
