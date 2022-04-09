47f4168b150a3c12a12e59b868142fd9388bcbde
kidow <wcgo2ling@gmail.com>
2022-04-09 18:15:35 +0900

    틀 짜기

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
+const plugin = require("tailwindcss/plugin");
+
+/** @typedef { import('tailwindcss/defaultConfig') } DefaultConfig */
+/** @typedef { import('tailwindcss/defaultTheme') } DefaultTheme */
+
+/** @type { DefaultConfig & { theme: { extend: DefaultTheme } } } */
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("prettier-plugin-tailwindcss")],
+  plugins: [
+    require("prettier-plugin-tailwindcss"),
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        ".tag": {},
+      });
+    }),
+  ],
 };
