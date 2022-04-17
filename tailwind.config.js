252d8db9a6d54e9105f5fc946180bd971419f489
kidow <wcgo2ling@gmail.com>
2022-04-18 02:27:10 +0900

    types tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 const plugin = require("tailwindcss/plugin");
 
-/** @typedef { import('tailwindcss/defaultConfig') } DefaultConfig */
-/** @typedef { import('tailwindcss/defaultTheme') } DefaultTheme */
-
-/** @type { DefaultConfig & { theme: { extend: DefaultTheme } } } */
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
