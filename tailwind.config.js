ecba101d3e6a041b0a9bcac18ce82f58279c5221
kidow <wcgo2ling@gmail.com>
2022-06-17 13:10:25 +0900

    내용 보완

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
-const plugin = require("tailwindcss/plugin");
+const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
   plugins: [
-    require("prettier-plugin-tailwindcss"),
+    require('prettier-plugin-tailwindcss'),
     plugin(function ({ addComponents, theme }) {
       addComponents({
-        ".tag": {
-          paddingTop: theme("spacing.[0.5]"),
-          paddingBottom: theme("spacing.[0.5]"),
-          paddingLeft: theme("spacing.2"),
-          paddingRight: theme("spacing.2"),
-          borderRadius: theme("borderRadius.xl"),
-          fontSize: theme("fontSize.sm"),
-          backgroundColor: theme("backgroundColor.gray.800"),
-          color: theme("colors.gray.300"),
+        '.tag': {
+          paddingTop: theme('spacing.[0.5]'),
+          paddingBottom: theme('spacing.[0.5]'),
+          paddingLeft: theme('spacing.2'),
+          paddingRight: theme('spacing.2'),
+          borderRadius: theme('borderRadius.xl'),
+          fontSize: theme('fontSize.sm'),
+          backgroundColor: theme('backgroundColor.gray.800'),
+          color: theme('colors.gray.300')
         },
-        ".content": {
-          color: theme("colors.neutral.400"),
-          lineHeight: theme("lineHeight.8"),
-        },
-      });
-    }),
-  ],
-};
+        '.content': {
+          color: theme('colors.neutral.400'),
+          lineHeight: theme('lineHeight.8')
+        }
+      })
+    })
+  ]
+}
